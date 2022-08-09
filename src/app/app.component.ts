b93e7bfe9f9f5508de717d2036248e3e3789ef90
Oliver Coimbra <olivercoimbra@gmail.com>
2022-08-09 19:23:46 -0400

    Modify function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   titulo: string = 'Contador App';
-  numero: number = 10
+  numero: number = 10;
+
+  // sumar(){
+  //   this.numero += 1
+  // }
+
+  // restar(){
+  //   this.numero -= 1
+  // }
+
+  acumular(valor: number) {
+    this.numero += valor
+  }
 }
