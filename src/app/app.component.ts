902b25e12b60e13e23b5028b56dd55bb16150273
Oliver Coimbra <olivercoimbra@gmail.com>
2022-08-10 12:15:21 -0400

    Add manual component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,18 +5,5 @@ import { Component } from '@angular/core';
   templateUrl: 'app.component.html'
 })
 export class AppComponent {
-  titulo: string = 'Contador App';
-  numero: number = 10;
-  base: number = 5;
-  // sumar(){
-  //   this.numero += 1
-  // }
 
-  // restar(){
-  //   this.numero -= 1
-  // }
-
-  acumular(valor: number) {
-    this.numero += valor
-  }
 }
