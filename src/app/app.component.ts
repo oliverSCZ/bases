ff9521dd0010caa5b403efad3e28e95169ea4406
Oliver Coimbra <olivercoimbra@gmail.com>
2022-08-10 10:30:56 -0400

    Add new feature base

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { Component } from '@angular/core';
 export class AppComponent {
   titulo: string = 'Contador App';
   numero: number = 10;
-
+  base: number = 5;
   // sumar(){
   //   this.numero += 1
   // }
